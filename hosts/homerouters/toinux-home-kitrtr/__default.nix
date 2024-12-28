b5e84c11b553812c52cd485ce0928f906d83444c
Antoine 'Toinux' Wam <root@nixos.local>
2024-12-28 19:02:56 +0100

    + hosts/homerouters
    + hosts/homerouters/aure-home-kitrtr/__default.nix:
    
    ~ hosts/homerouters/aure-home-kitrtr/default.nix:
    
    + hosts/homerouters/aure-home-kitrtr/network-configuration.nix:
    
    ~ hosts/homerouters/aure-home-kitrtr/peers/KIT-IG1-RTR.nix:
    
    ~ hosts/homerouters/default.nix:
    
    + hosts/homerouters/romain-home-kitrtr/_default.nix:
    
    ~ hosts/homerouters/romain-home-kitrtr/default.nix:
    
    + hosts/homerouters/romain-home-kitrtr/network-configuration.nix:
    
    ~ hosts/homerouters/romain-home-kitrtr/peers/KIT-IG1-RTR.nix:
    
    ~ hosts/homerouters/romain-home-kitrtr/peers/default.nix:
    
    + hosts/homerouters/toinux-home-kitrtr/__default.nix:
    
    ~ hosts/homerouters/toinux-home-kitrtr/default.nix:
    
    + hosts/homerouters/toinux-home-kitrtr/network-configuration.nix:
    
    ~ hosts/homerouters/toinux-home-kitrtr/peers/default.nix:

---
+++ hosts/homerouters/toinux-home-kitrtr/__default.nix
@@ -0,0 +1,24 @@
+{ ... }:
+{
+  bootdisk = "/dev/vda";
+  diskTemplate = "simple_singleFullRoot";
+
+  interface = "ens18";
+  # mainSerial = 0;
+
+  birdConfig = {
+    # inherit transitInterface;
+
+    # router-id = ;
+
+    # loopback4 = "";
+
+    static6 = [
+      "::/0 recursive 2a13:79c0:ffff:fefe::b00b"
+
+      #"2a13:79c0:ffff::/48 unreachable" # Networking stuff
+      #"2a13:79c0:ffff:fefe::/64 unreachable" # LoopBacks
+      #"2a13:79c0:ff00::/40 unreachable" # full range /40
+    ];
+  };
+}
