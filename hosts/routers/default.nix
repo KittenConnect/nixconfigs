4e7abafffcf8803b1f46d4b43af2878e50c69d0a
Antoine 'Toinux' Wam <root@nixos.local>
2024-12-29 17:04:13 +0100

    Cleanup

--- hosts/routers/default.nix
+++ hosts/routers/default.nix
@@ -1,18 +1,55 @@
-# {
-#   iguane-kit-rtr = import ./iguane-kit-rtr { };
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-#   vultr-kit-edge = import ./vultr-kit-edge { };
-#   virtua-kit-edge = import ./virtua-kit-edge { };
-# }
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
-# args@{ lib, ... }:
-# let
-#   blacklist = [ ];
-#   folders = builtins.attrNames (
-#     lib.filterAttrs (n: v: v == "directory" && !lib.hasPrefix "_" n && !builtins.elem n blacklist) (
-#       builtins.readDir ./.
-#     )
-#   );
-# in
-# lib.genAttrs folders (folder: (import (./. + "/${folder}") (args // { })))
-{...}: {}
+{
+  imports = [
+    # Include the results of the hardware scan.
+    # ./bird.nix # Bird Routing
+    # ./wireguard.nix
+    # ./firewall.nix
+  ];
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Net Basics
+  networking.useNetworkd = true;
+  systemd.network.enable = true;
+  systemd.network.wait-online.enable = false;
+
+  # List services that you want to enable:
+  services.bird2 = {
+    enable = true;
+    autoReload = true;
+  };
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
