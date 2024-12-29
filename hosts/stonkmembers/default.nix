07a168e9ea26cb07e975490a2ea4aa62b20c7f36
Antoine 'Toinux' Wam <root@nixos.local>
2024-12-29 16:48:37 +0100

    - hosts/stonkmembers/configuration.nix:

--- hosts/stonkmembers/default.nix
+++ hosts/stonkmembers/default.nix
@@ -1,18 +1,32 @@
-# {
-#   poubelle00 = import ./poubelle00 { };
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-#   prodesk = import ./prodesk { };
-#   stonkstation = import ./stonkstation { };
-# }
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
-# args@{ lib, ... }:
-# let
-#   blacklist = [ ];
-#   folders = builtins.attrNames (
-#     lib.filterAttrs (n: v: v == "directory" && !lib.hasPrefix "_" n && !builtins.elem n blacklist) (
-#       builtins.readDir ./.
-#     )
-#   );
-# in
-# lib.genAttrs folders (folder: (import (./. + "/${folder}") (args // { })))
-{...}: {}
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./k3s.nix # K3s
+  ];
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # List services that you want to enable:
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+}
