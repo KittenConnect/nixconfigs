a74def351b04a300c895878fdfeca23c135ab0b9
Antoine 'Toinux' Wam <root@nixos.local>
2024-12-29 16:48:37 +0100

    - hosts/miscservers/configuration.nix:

--- hosts/miscservers/default.nix
+++ hosts/miscservers/default.nix
@@ -1,12 +1,28 @@
-# args@{ lib, ... }:
-# let
-#   blacklist = [ ];
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-#   folders = builtins.attrNames (
-#     lib.filterAttrs (n: v: v == "directory" && !lib.hasPrefix "_" n && !builtins.elem n blacklist) (
-#       builtins.readDir ./.
-#     )
-#   );
-# in
-# lib.genAttrs folders (folder: (import (./. + "/${folder}") (args // { })))
-{...}: {}
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [ ];
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Net Basics
+  networking.useNetworkd = true;
+  systemd.network.enable = true;
+  systemd.network.wait-online.enable = false;
+
+  # environment.systemPackages = with pkgs; [ ];
+}
