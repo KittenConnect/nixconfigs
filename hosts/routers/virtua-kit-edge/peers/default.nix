914da28f3ab8d67851b5abdd6b4da4a8fee502df
Antoine 'Toinux' Wam <root@nixos.local>
2024-12-29 16:48:37 +0100

    ~ hosts/routers/virtua-kit-edge/peers/default.nix:

--- hosts/routers/virtua-kit-edge/peers/default.nix
+++ hosts/routers/virtua-kit-edge/peers/default.nix
@@ -1,16 +1,17 @@
-{ ... }:
-let
-  defaultPeers = import ../../_peers { };
-in
-defaultPeers
-// {
+{ kittenLib, ... }:
+kittenLib.peers {
+  host = ./.;
+  profile = ../..;
 
-  # Transit
-  TRS_virtua6_RS01 = import ./TRS-virtua6-RS01.nix { };
-  TRS_virtua6_RS02 = import ./TRS-virtua6-RS02.nix { };
+  blacklist = [ "KIT-VIRTUA-EDGE.legacy" ];
+  manual = {
+    # Transit
+    TRS_virtua6_RS01 = ./TRS-virtua6-RS01.nix;
+    TRS_virtua6_RS02 = ./TRS-virtua6-RS02.nix;
 
-  # Internal Tunnels
-  KIT_IG1_RTR = import ./KIT-IG1-RTR.nix { };
-  vultrNix_PAR = import ./KIT-vultr-edge.nix { };
-  # LGC_virtua_PAR = import ./KIT-VIRTUA-EDGE.legacy.nix { };
+    # Internal Tunnels
+    KIT_IG1_RTR = ./KIT-IG1-RTR.nix;
+    vultrNix_PAR = ./KIT-vultr-edge.nix;
+    # LGC_virtua_PAR = ./KIT-VIRTUA-EDGE.legacy.nix;
+  };
 }
