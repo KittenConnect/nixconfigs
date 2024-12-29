5b74bc27923d88025edaacf48480306cb64b8319
Antoine 'Toinux' Wam <root@nixos.local>
2024-12-29 16:48:37 +0100

    ~ hosts/miscservers/aure-kit-bots-01/default.nix:

--- hosts/miscservers/aure-kit-bots-01/default.nix
+++ hosts/miscservers/aure-kit-bots-01/default.nix
@@ -1,35 +1,108 @@
-{ ... }:
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 {
-  type = "targetConfig";
+  name,
+  nodes,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  diskoProfile = "simple";
+  diskoConfig = {
+    bootdisk = "/dev/vda";
+  };
+in
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ../default.nix
+    ./hardware-configuration.nix
+    # ./network-configuration.nix # TODO: implement
+  ];
 
-  bootdisk = "/dev/vda";
-  diskTemplate = "simple_singleFullRoot";
-
-  # mainSerial = 0;
-
-  config = {
-    hostprofile.miscservers = {
-      interface = "ens18";
-    };
+  deployment = {
+    # Disable SSH deployment. This node will be skipped in a
+    # normal`colmena apply`.
+    targetUser = "root";
+    targetHost = null; # TODO: define me
   };
 
-  # birdConfig = {
-  #   # inherit transitInterface;
+  # Bootloader.
+  boot.loader.grub.efiSupport = false;
+  boot.loader.grub.enable = true;
 
-  #   # router-id = ;
 
-  #   # loopback4 = "";
-  #   loopback6 = "2a13:79c0:ffff:fefe::22f0";
+  kittenModules = {
+    # network = {
+    #   enable = true;
+    #   interface = "ens18";
+    #   address = "";
+    # };
 
-  #   static6 = [
-  #     "::/0 recursive 2a13:79c0:ffff:fefe::b00b"
+    disko = {
+      enable = true;
+      profile = diskoProfile;
+      ${diskoProfile} = diskoConfig;
+    };
 
-  #     # "2a13:79c0:ffff:feff:b00b:caca:b173:0/112 unreachable" # Direct on ens19
-  #     "2a13:79c0:fffe:100::/56 unreachable"
+    # firewall = {
+    #   enable = true;
+    #   forward = {
+    #     enable = true;
+    #     # stateless = true;
+    #     rules = ''
+    #       iifname $wireguardIFACEs oifname "vlan36" ip6 daddr 2a13:79c0:ffff:feff:b00b:3615:1:0/112 counter accept
+    #       oifname $wireguardIFACEs iifname "vlan36" ip6 saddr 2a13:79c0:ffff:feff:b00b:3615:1:0/112 counter accept
+    #     '';
+    #   };
+    # };
+  };
+  systemd.network.enable = true;
 
-  #     #"2a13:79c0:ffff::/48 unreachable" # Networking stuff
-  #     #"2a13:79c0:ffff:fefe::/64 unreachable" # LoopBacks
-  #     #"2a13:79c0:ff00::/40 unreachable" # full range /40
-  #   ];
-  # };
+  nixpkgs.config.allowUnfree = true;
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
+  programs.mtr.enable = true;
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ ];
+  networking.firewall.allowedUDPPorts = [ ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = lib.mkDefault true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
 }
