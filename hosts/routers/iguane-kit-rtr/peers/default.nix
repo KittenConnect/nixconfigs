1589f07de3361e66a3b3a44d14fc48e8c20393c8
Antoine 'Toinux' Wam <root@nixos.local>
2024-12-29 16:48:37 +0100

    ~ hosts/routers/iguane-kit-rtr/peers/default.nix:

--- hosts/routers/iguane-kit-rtr/peers/default.nix
+++ hosts/routers/iguane-kit-rtr/peers/default.nix
@@ -1,21 +1,18 @@
-{ ... }:
-let
-  profilePeers = import ../../_peers { };
-in
-profilePeers
-// {
+{ kittenLib, ... }:
+kittenLib.peers {
+  host = ./.;
+  profile = ../..;
 
-  # Transit
-  # TRS_virtua6_RS01 = import ./TRS-virtua6-RS01.nix { };
-  # TRS_virtua6_RS02 = import ./TRS-virtua6-RS02.nix { };
+  blacklist = [ ];
+  manual = {
+    # Internal Tunnels
+    virtuaNix_PAR = ./KIT-VIRTUA-EDGE.nix;
+    vultrNix_PAR = ./KIT-VULTR-EDGE.nix;
+    # LGC_virtua_PAR = ./KIT-VIRTUA-EDGE.legacy.nix;
 
-  # Internal Tunnels
-  virtuaNix_PAR = import ./KIT-VIRTUA-EDGE.nix { };
-  vultrNix_PAR = import ./KIT-VULTR-EDGE.nix { };
-  # LGC_virtua_PAR = import ./KIT-VIRTUA-EDGE.legacy.nix { };
-
-  aureG8 = import ./KIT-aurelien-RBR.nix { };
-  toinuxMEL1 = import ./KIT-toinux-MEL1.nix { };
-  roumainNTE = import ./KIT-roumain-NTE.nix { };
-  roumaiNixNTE = import ./KIT-roumainNix-NTE.nix { };
+    aureG8 = ./KIT-aurelien-RBR.nix;
+    toinuxMEL1 = ./KIT-toinux-MEL1.nix;
+    roumainNTE = ./KIT-roumain-NTE.nix;
+    roumaiNixNTE = ./KIT-roumainNix-NTE.nix;
+  };
 }
