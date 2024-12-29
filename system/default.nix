b94c9dcadde9822c62c8b5f0968c21a0501ce27c
Antoine 'Toinux' Wam <root@nixos.local>
2024-12-29 18:34:05 +0100

    currentSystemPackages in /etc includes npins versions/commits

--- system/default.nix
+++ system/default.nix
@@ -6,6 +6,7 @@
   config,
   lib,
   pkgs,
+  sources,
   ...
 }:
 
@@ -63,10 +64,28 @@
   # Versions Dump
   environment.etc."current-system-packages".text =
     let
+      channelUrlRev =
+        s:
+        lib.replaceStrings [ "/" ] [ "_" ] (
+          lib.removePrefix "https://releases.nixos.org/" (lib.removeSuffix "/nixexprs.tar.xz" s)
+        );
+
+      sourceRevision =
+        source:
+        if source ? revision then
+          "${source.version or source.branch}_${source.revision}"
+        else
+          (if source.type == "Channel" then channelUrlRev source.url else source.hash);
+
       getName = (p: if p ? name then "${p.name}" else "${p}");
       packages = builtins.map getName config.environment.systemPackages;
-      sortedUnique = builtins.sort builtins.lessThan (lib.unique packages);
-      formatted = builtins.concatStringsSep "\n" sortedUnique;
+      sortedUnique = l: builtins.sort builtins.lessThan (lib.unique l);
+
+      npinSources = builtins.sort builtins.lessThan (
+        lib.mapAttrsToList (n: v: "npins-sources-${n}-${sourceRevision v}") sources
+      );
+
+      formatted = builtins.concatStringsSep "\n" ((sortedUnique npinSources) ++ [] ++ (sortedUnique packages));
     in
     formatted;
 }
