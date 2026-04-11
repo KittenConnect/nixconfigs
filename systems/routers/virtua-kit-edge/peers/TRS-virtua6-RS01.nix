16446b6de96d9806c37bfa6a767f57913b51dbb4
toinux <26522723+itzwam@users.noreply.github.com>
2026-04-11 14:11:44 +0200

    feat: virtua use new ASN+INET6

--- systems/routers/virtua-kit-edge/peers/TRS-virtua6-RS01.nix
+++ systems/routers/virtua-kit-edge/peers/TRS-virtua6-RS01.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-  localAS = 207175;
+  localAS = 213197;
   peerAS = 35661;
   peerIP = "2a0d:e680:0::b:1";
   multihop = 5;
@@ -10,7 +10,7 @@
   ipv6 = {
     bgpImports = null;
     bgpExports = [
-      "2a13:79c0:ff00::/40" # Prod /40
+      "2a12:5844:1310::/44" # AS 
 
       # "2a12:dd47:9330::/44"
     ];
