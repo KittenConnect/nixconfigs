80c271a8b643dc539eeffd703538f1d2ce95b3b8
toinux <26522723+itzwam@users.noreply.github.com>
2026-04-11 14:13:53 +0200

    fix: vultr new ASN+INET6

---
+++ systems/routers/vultr-kit-edge/peers/TRS-vultr6-RTR.nix
@@ -0,0 +1,16 @@
+{ ... }:
+{
+  localAS = 213197;
+  peerAS = 64515;
+  peerIP = "2001:19f0:ffff::1";
+  multihop = 2;
+
+  passwordRef = "vultr";
+
+  ipv6 = {
+    bgpImports = null;
+    bgpExports = [
+      "2a12:5844:1310::/44" # Kitten Public IPv6
+    ];
+  };
+}
