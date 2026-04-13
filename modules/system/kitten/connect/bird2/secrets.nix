5edd3f59f26470a720c954d68ff05bd01e9b92d6
Antoine 'toinux' Wam <toinux@maison.local>
2026-04-13 13:54:22 +0200

    feat: Bird module rework

---
+++ modules/system/kitten/connect/bird2/secrets.nix
@@ -0,0 +1,92 @@
+args@{
+  lib,
+  kittenLib,
+  pkgs,
+  config,
+  name,
+  ...
+}:
+let
+  inherit (lib)
+    optional
+    optionals
+    optionalString
+    mkOrder
+    mkDefault
+    attrNames
+    filterAttrs
+    concatStringsSep
+    concatMapStringsSep
+    listToAttrs
+    nameValuePair
+    mkMerge
+    mkIf
+    mkOption
+    mkEnableOption
+    types
+    unique
+    mapAttrsToList
+    isType
+    ;
+
+  inherit (kittenLib.strings) indentedLines quotedString;
+
+  withType = types: x: lib.toFunction types.${builtins.typeOf x} x;
+
+  # Main config is here
+  cfg = config.kittenModules.bird;
+
+  # Values
+  peers = cfg.peers;
+  peersWithPasswordRef = filterAttrs (n: v: v.passwordRef != null) peers;
+
+  passwords = unique (mapAttrsToList (n: v: v.passwordRef) peersWithPasswordRef);
+in
+{
+  # Sops secrets implementation
+  config = mkIf cfg.enable {
+    # Secrets management
+    sops = mkIf (passwords != [ ]) {
+      secrets = (
+        listToAttrs (
+          map (n: nameValuePair "bird_secrets/${n}" { reloadUnits = [ "bird2.service" ]; }) passwords
+        )
+      );
+
+      templates."bird_secrets.conf" = mkIf (passwords != [ ]) {
+        owner = cfg.user;
+        content = (
+          mkMerge (
+            map (password: ''
+              define secretPassword_${password} = "${config.sops.placeholder."bird_secrets/${password}"}";
+            '') passwords
+          )
+        );
+      };
+    };
+
+    # Service configuration
+    services.bird2 = {
+      enable = cfg.enable;
+      
+      preCheckConfig = mkIf (passwords != [ ]) ''
+        (
+            LINE=$(grep -n include bird2.conf | grep bird_secrets.conf | head -1 | cut -d: -f1)
+            if [ ! -z "$LINE" ]; then
+                echo "Found secrets importing, will substitute it with placeholders values"
+                sed ''${LINE}d -i bird2.conf
+                sed "$(($LINE))i"'include "_secrets_substitute.conf";' -i bird2.conf
+
+                cat > _secrets_substitute.conf <<< '
+                    ${config.sops.templates."bird_secrets.conf".content}
+                '
+            fi
+        )
+      '';
+
+      config = mkIf (passwords != [ ]) (
+          mkOrder 5 ''include "${config.sops.templates."bird_secrets.conf".path}";''
+        );
+    };
+  };
+}
