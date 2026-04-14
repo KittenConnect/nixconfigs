d9a72b3cc3576e867ecb7db4770de087d0d9388e
toinux <26522723+itzwam@users.noreply.github.com>
2026-04-14 14:31:37 +0200

    chore: renumber in filter functs

--- systems/homerouters/aure-home-kitrtr/default.nix
+++ systems/homerouters/aure-home-kitrtr/default.nix
@@ -66,7 +66,7 @@ in {
         "::/0 recursive 1010:cafe:ffff:fefe::b00b"
 
         # "1010:cafe:ffff:feff:b00b:caca:b173:0/112 unreachable" # Direct on ens19
-        "2a13:79c0:fffe:100::/56 unreachable"
+        # "2a13:79c0:fffe:100::/56 unreachable"
 
         #"2a13:79c0:ffff::/48 unreachable" # Networking stuff
         #"1010:cafe:ffff:fefe::/64 unreachable" # LoopBacks
