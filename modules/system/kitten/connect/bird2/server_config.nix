87cecbc82c9711ba2af782681b6d70e4a9ab1ac3
Antoine 'toinux' Lesieur <antoine.lesieur@s3ns.io>
2026-04-14 05:11:40 +0200

    chore: bird kernel conf rework

--- modules/system/kitten/connect/bird2/server_config.nix
+++ modules/system/kitten/connect/bird2/server_config.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  kittenLib,
   config,
   birdFuncs,
   ...
@@ -17,81 +18,48 @@
     concatMapStringsSep
     ;
 
+  inherit (kittenLib.strings) indentedLines;
+
   inherit (birdFuncs) quotedString;
 
   birdCfg = config.services.bird2;
   srvCfg = config.kittenModules.bird;
 
   rrs = attrNames (filterAttrs (n: v: v ? template && v.template == "rrserver") srvCfg.peers);
+  RRs = "proto ~ [${concatMapStringsSep ", " quotedString rrs}]";
+
+  setLoopBackSRC = nets: loopback: ''
+    if source ~ [RTS_BGP] || net ~ [ ${nets} ] then {
+      krt_prefsrc=${loopback};
+    }'';
 in {
-  services.bird2.config = mkMerge [
-    (
-      mkOrder 25 ''
+  services.bird2.config = mkOrder 25 ''
+    # The Kernel protocol is not a real routing protocol. Instead of communicating
+    # with other routers in the network, it performs synchronization of BIRD
+    # routing tables with the OS kernel. One instance per table.
+    protocol kernel KERNEL4 {
+      ipv4 {
+        export filter {
+          if  ( is_valid4_network() || source ~ [RTS_STATIC] ${optionalString (rrs != []) "|| ${RRs}"}) then {
+    ${optionalString (srvCfg.loopback4 != null && srvCfg.loopback4 != "") (indentedLines 4 (setLoopBackSRC "0.0.0.0/0" srvCfg.loopback4))}
+              accept;
+          } else reject;
+        };
+      };
+      merge paths on;
+    }
 
-        # The Kernel protocol is not a real routing protocol. Instead of communicating
-        # with other routers in the network, it performs synchronization of BIRD
-        # routing tables with the OS kernel. One instance per table.
-        protocol kernel KERNEL4 {
-        	ipv4 {                  # Connect protocol to IPv4 table by channel
-        #             table master4;    # Default IPv4 table is master4
-        #             import all;       # Import to table, default is import all
-        #             export all;       # Export to protocol. default is export none
-        	      export filter {
-        		  if  ( is_valid4_network() || source ~ [RTS_STATIC]
-              ${
-          let
-            sep = "|| proto =";
-          in
-            optionalString (rrs != []) sep + (concatMapStringsSep sep quotedString rrs)
-        }
-              ) then {
-                  ${
-          optionalString (srvCfg.loopback4 != null && srvCfg.loopback4 != "") ''
-
-              if source ~ [RTS_BGP] || net ~ [ 0.0.0.0/0 ] then {
-            krt_prefsrc=${srvCfg.loopback4};
-              }
-          ''
-        }
-        		     accept;
-        		  } else reject;
-        	      };
-        	};
-          merge paths on;
-        #       learn;                  # Learn alien routes from the kernel
-        #       kernel table 10;        # Kernel table to synchronize with (default: main)
-        }
-
-        # Another instance for IPv6, skipping default options
-        protocol kernel KERNEL6 {
-        #       ipv6 { export all; };
-        	ipv6 {
-        	     export filter {
-
-               if  ( is_valid6_network() || source ~ [RTS_STATIC]
-              ${
-          let
-            sep = "|| proto =";
-          in
-            optionalString (rrs != []) sep + (concatMapStringsSep sep quotedString rrs)
-        }
-              ) then {
-                           ${
-          optionalString (srvCfg.loopback6 != null && srvCfg.loopback6 != "") ''
-
-               if source ~ [RTS_BGP] || net ~ [ ::/0 ] then {
-            krt_prefsrc=${srvCfg.loopback6};
-               }
-          ''
-        }
-        		       accept;
-        		 } else reject;
-        	     };
-        	};
-
-          merge paths on;
-        }
-      ''
-    )
-  ];
+    # Another instance for IPv6, skipping default options
+    protocol kernel KERNEL6 {
+      ipv6 {
+        export filter {
+          if (is_valid6_network() || source ~ [RTS_STATIC] ${optionalString (rrs != []) "|| ${RRs}"}) then {
+    ${optionalString (srvCfg.loopback6 != null && srvCfg.loopback6 != "") (indentedLines 4 (setLoopBackSRC "::/0" srvCfg.loopback6))}
+            accept;
+          } else reject;
+        };
+      };
+      merge paths on;
+    }
+  '';
 }
