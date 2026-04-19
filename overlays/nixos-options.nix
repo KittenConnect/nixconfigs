3274afa45b89987fd09d2002ab3d14b3765c3a94
Antoine 'toinux' Wam <toinux@maison.local>
2026-04-19 18:06:18 +0200

    fix: nixos-options

--- overlays/nixos-options.nix
+++ overlays/nixos-options.nix
@@ -1,12 +1,19 @@
-(final: prev: {
-  nixos-option = let
-    prefix = ''(import /etc/nixos/ci/hive.nix).nodes.\$(hostname)'';
-  in
-    prev.runCommandNoCC "nixos-option" {buildInputs = [prev.makeWrapper];} ''
-      makeWrapper ${prev.nixos-option}/bin/nixos-option $out/bin/nixos-option \
-        --add-flags --config_expr \
-        --add-flags "\"${prefix}.config\"" \
-        --add-flags --options_expr \
-        --add-flags "\"${prefix}.options\""
-    '';
-})
+final: prev: {
+  nixos-option =
+    let
+      prefix = ''(import /etc/nixos/ci/hive.nix).nodes.\$(hostname)'';
+    in
+    prev.nixos-option.overrideAttrs (
+      finalAttrs: prevAttrs: {
+        buildInputs = (prev.buildInputs or [ ]) ++ [ prev.makeWrapper ];
+
+        postFixup = ''
+          wrapProgram $out/bin/nixos-option \
+          --add-flags --config_expr \
+          --add-flags "\"${prefix}.config\"" \
+          --add-flags --options_expr \
+          --add-flags "\"${prefix}.options\""
+        '';
+      }
+    );
+}
