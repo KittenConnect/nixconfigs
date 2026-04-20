563dadc5b51e07996be994ca5a5fbb9164e98357
toinux <26522723+itzwam@users.noreply.github.com>
2026-04-20 19:51:17 +0200

    feat: die flake

--- modules/system/kitten/connect/disko/default.nix
+++ modules/system/kitten/connect/disko/default.nix
@@ -34,5 +34,23 @@ in {
     ./profiles
   ];
 
-  config = lib.mkIf (cfg.enable) {};
+  config = lib.mkIf (cfg.enable) {
+    system.build = {
+      diskoImagesCompressed = pkgs.runCommand "compressed-disko-${confName}" {
+            # TODO: see if need prefer local + structuredAttrs + unsafe discard
+          } ''
+            pwd
+
+            xz="${pkgs.xz}/bin/xz"
+
+            mkdir -pv $out
+            cd ${config.system.build.diskoImages}
+
+            echo Compressing disk images with xz
+            echo CAUTION: May take some times
+
+            find . -name '*.raw' -print -exec bash -c "$xz -T0 --stdout '{}' > '$out/{}.xz'" \;
+          ''
+    };
+  };
 }
