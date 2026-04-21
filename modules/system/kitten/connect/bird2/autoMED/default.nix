3780371428eaea2154eb9ae0bd65b2673393c61a
Antoine 'toinux' Wam <toinux@maison.local>
2026-04-21 05:36:00 +0200

    fix: ryanAir

---
+++ modules/system/kitten/connect/bird2/autoMED/default.nix
@@ -0,0 +1,78 @@
+args @ {
+  lib,
+  kittenLib,
+  pkgs,
+  config,
+  name,
+  ...
+}: let
+  inherit
+    (lib)
+    mkOrder
+    filterAttrs
+    listToAttrs
+    nameValuePair
+    mkMerge
+    mkIf
+    unique
+    mapAttrsToList
+    ;
+
+  # Main config is here
+  cfg = config.kittenModules.bird;
+
+  # Values
+  peers = cfg.peers;
+  peersWithAutoMED = filterAttrs (n: v: v.bgpMED == -1) peers;
+
+  autoMEDPath = "/run/bird/automed.conf";
+  autoMEDPackage = pkgs.callPackage ./package.nix {};
+in {
+  # Sops secrets implementation
+  config = lib.mkMerge [
+    (mkIf (!cfg.autoMED) {
+      assertions = [
+        {
+          assertion = peersWithAutoMED == {};
+          message = "Cannot have peers with MED == -1 (autoMED) if config.kittenModules.bird.automed daemon is disabled";
+        }
+      ];
+      # throw "TODO: implement auto bgpMED"
+    })
+
+    (mkIf (cfg.autoMED) {
+      # Service configuration
+      systemd.services.bird-icmp-automed = {
+        wants = ["network-online.target"];
+        after = ["network-online.target"];
+
+        serviceConfig = {
+          Type = "simple";
+          SupplementaryGroups = ["bird"];
+          AmbientCapabilities = ["CAP_NET_RAW" "CAP_NET_ADMIN"];
+          ExecStart = "${autoMEDPackage}/bin/bird-icmp-automed ${autoMEDPath}";
+        };
+      };
+
+      # Bird configuration
+      services.${cfg.serviceName} = {
+        preCheckConfig = ''
+          (
+              LINE=$(grep -n include ${cfg.serviceName}.conf | grep ${autoMEDPath} | head -1 | cut -d: -f1)
+              if [ ! -z "$LINE" ]; then
+                  echo "Found autoMED importing, will substitute it with placeholders values"
+                  sed ''${LINE}d -i ${cfg.serviceName}.conf
+                  sed "$(($LINE))i"'include "_automed_substitute.conf";' -i ${cfg.serviceName}.conf
+
+                  cat > _automed_substitute.conf <<< '
+                      ${lib.concatMapStringsSep "\n" (x: ''define bgpMED_${x} = 999999;'') (builtins.attrNames peersWithAutoMED)}
+                  '
+              fi
+          )
+        '';
+
+        config = mkOrder 5 ''include "${autoMEDPath}";'';
+      };
+    })
+  ];
+}
---
+++ modules/system/kitten/connect/bird2/autoMED/package.nix
@@ -0,0 +1,6 @@
+{buildGoModule, ...}:
+buildGoModule {
+  name = "bird-icmp-automed";
+  vendorHash = "sha256-RpYRk+cKf6NODZu/4zLdNF/C8sSUWiIkKEXFk035cTA=";
+  src = ./src;
+}
